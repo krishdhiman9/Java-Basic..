4ceb202b4bcbb484e1897b9fee911f55227ef0ef
kd <krishdhiman86@gmail.com>
2026-03-01 21:47:44 +0530

    first commit

---
+++ variables.java
@@ -0,0 +1,17 @@
+public class variables {
+    public static void main(String[] args) {
+    //variables
+    int age = 77;
+    char hello = 'k';
+    String name = "kname";
+    double age1 = 56.56;
+    boolean bf = true;
+    String lodu = "krish.l";
+    System.out.println(age);
+    System.out.println(hello);
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(bf);
+    System.out.println(lodu);
+    }
+}
