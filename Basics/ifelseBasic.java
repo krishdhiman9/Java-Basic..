a24a4d751c3a9e6de70aad59f1a3e0ca68f04aee
kd <krishdhiman86@gmail.com>
2026-03-02 23:13:01 +0530

    added Basics folder

---
+++ Basics/ifelseBasic.java
@@ -0,0 +1,36 @@
+package Basics;
+
+public class ifelseBasic {
+    public static void main(String[] args) {
+        
+        //if statement
+        int age = 18;
+        if (age>=18){
+            System.out.println("eligible for voting");
+        }
+
+        //if-else statement
+        int number = 5;
+        if (number>0){
+            System.out.println("positive number");
+        }
+            else {
+            System.out.println("not positve number");
+        }
+        }
+        
+        //else-if statement
+        int num = 0;{
+
+if (num > 0) {
+    System.out.println("Positive");
+}
+else if (num < 0) {
+    System.out.println("Negative");
+}
+else {
+    System.out.println("Zero");
+}
+    }
+}
+
