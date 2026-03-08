05b42ea4250777b1143f698b62ef1300932f19ed
kd <krishdhiman86@gmail.com>
2026-03-08 22:48:30 +0530

    practice question

--- oops/QcreateSquare.java
+++ oops/QcreateSquare.java
@@ -1,19 +1,24 @@
 package oops;
-class square{
+class Square{
     int side;
+    //question is create a class square with a method to initize its side, calculate area ,perimeter.
+    public void area(){
+        System.out.println("area of sqaure: " + (side*side));
+    }
 
-    
-
-
+    public void perimeter(){
+        System.out.println("perimeter of sqaure: " + (4*side));
+    }
 
 }
-
-
-
-
 public class QcreateSquare {
     
     public static void main(String[] args) {
         
+
+        Square box = new Square();
+        box.side=2;
+        box.area();
+        box.perimeter();
     }
 }
