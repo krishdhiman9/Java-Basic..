3f326eef7a7ce565f01b38001f155b81a204d897
kd <krishdhiman86@gmail.com>
2026-03-10 22:11:23 +0530

    oops

---
+++ oops/QCellphn.java
@@ -0,0 +1,22 @@
+package oopsConcept;
+class CellPhone{
+    //problem is => create a class cellphone with mwthods to print "ringing",,"vibrating"...
+    
+    
+    public void ringing(){
+        System.out.println("your phone is ringing");
+    }
+    public void vibrating(){
+        System.out.println("your phone is vibrating...zzzzz");
+    }
+    
+}
+
+public class QCellphn{
+    public static void main(String[] args) {
+    
+        CellPhone hello = new CellPhone();
+        hello.ringing();
+        hello.vibrating();
+    }
+}
---
+++ oops/oopsbasics/oopsBasics.java
@@ -0,0 +1,50 @@
+package oops.oopsbasics;
+
+//package oopsbasics;
+
+
+// a class (only one class create in program)
+class employee{
+    int id;
+    String name;
+    int age;
+    int salary;
+
+    //creating a method/function.
+    public void getDetails(){
+        System.out.println("my id is " + id);
+        System.out.println("my name is.. " + name);
+        System.out.println("age is.. "+ age);
+        System.out.println("salary is " + salary);
+    }
+}
+public class oopsBasics {
+    public static void main(String[] args) {
+
+        //creating  object
+        employee Rahul = new employee();
+        employee Harry = new employee();
+
+        //assigning values to object
+        Rahul.name = "Rahul Sronia";
+        Rahul.id = 11;
+        Rahul.age = 17;
+        Rahul.salary=20000;
+
+
+        Harry.name = "Harry Sandhu";
+        Harry.id = 12;
+        Harry.age = 18;
+        Harry.salary = 21000;
+
+
+        //System.out.println(Rahul.id);
+        //System.out.println(Harry.id);
+        //System.out.println(Rahul.name);
+        //System.out.println(Harry.name);
+
+        //using function to print the values.
+        Rahul.getDetails();
+        Harry.getDetails();    
+    }
+}
