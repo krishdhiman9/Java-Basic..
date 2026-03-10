ada050d5ed824cc3a49299a94716b7ef642cc349
kd <krishdhiman86@gmail.com>
2026-03-10 23:48:00 +0530

    questions

---
+++ javaQuestions/checkPositiveNegitiveZero.java
@@ -0,0 +1,24 @@
+package javaQuestions;
+import java.util.Scanner;
+public class checkPositiveNegitiveZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number " );
+        int num = sc.nextInt();
+
+        //System.out.println("enter number " + num);
+
+        if( num>0){
+            System.out.println("positive number ");
+        }
+
+        else if(num<0){
+            System.out.println("negitive number ");
+        }
+
+        else {
+            System.out.println("number is zero");
+
+        }
+    }
+}
