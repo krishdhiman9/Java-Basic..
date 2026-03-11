92bb1a13fe4d6e44a645268dc37ef85e1128d206
kd <krishdhiman86@gmail.com>
2026-03-11 15:45:29 +0530

    practice set

--- javaPracQuestion/claculator.java
+++ javaPracQuestion/claculator.java
@@ -9,31 +9,30 @@ public class claculator {
 
         System.out.print("enter first number: ");
         int a = sc.nextInt();
-        System.out.print("enter number 1,2,3,4: ");
-        int c = sc.nextInt();
+        System.out.print("enter operator: ");
+        char c = sc.next().charAt(0);
         System.out.print("enter 2nd number: ");
         int b = sc.nextInt();
         int result=0;
 
 
         switch (c){
-            case 1:
+            case '+':
                 result=a+b;
                 break;
-            case 2:
+            case '-':
                 result=a-b;
                 break;
-            case 3:
+            case '*':
                 result=a*b;
                 break;
-            case 4:
+            case '/':
                 result=a/b;
                 break;
-            
-
-
-
-        }
+            case '%':
+                result=a%b;
+                break;
+            }
         System.out.println("your answer is: " + result);
     }
     
