e39603395e2058a5486a7c8864a7006b516e0af9
kd <krishdhiman86@gmail.com>
2026-03-13 23:19:09 +0530

    setter & getter

---
+++ oops/accessModifiers/basics.java
@@ -0,0 +1,34 @@
+package accessModifiers;
+class Myemployee{
+    private int id;
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int i){
+        id = i;
+    }
+}
+public class basics {
+    public static void main(String[] args) {
+        Myemployee krish = new Myemployee();
+        //krish.id=34;
+        //krish.name=("krish dhiman ");
+        krish.setName("krish dhiman");
+        System.out.println(krish.getName());
+
+        krish.setId(132);
+        System.out.println(krish.getId());
+
+
+    }
+    
+}
