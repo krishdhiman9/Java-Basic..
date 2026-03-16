78abe3101933412eb03cdd80b27fb6b6c45972ac
kd <krishdhiman86@gmail.com>
2026-03-16 21:36:28 +0530

    practice Q

---
+++ oops/accessModifiers/Qconstructor.java
@@ -0,0 +1,42 @@
+package accessModifiers;
+class setCylinder{
+    private int radius ;
+    private int height;
+
+    public setCylinder(){
+        radius=22;
+        height =33;
+}
+    //public setCylinder(int Radius,int Height){
+     //   Radius = radius;
+       // Height = height;
+    //}
+    
+    public int getRadius(){
+        return radius;
+    }
+    public void setRadius(int r){
+        radius = r;
+    }
+
+    public int getHeight(){
+        return height;
+    }
+    public void setHeight(int h){
+        height = h;
+    }
+}
+
+public class Qconstructor {
+    public static void main(String[] args) {
+        setCylinder Cy = new setCylinder();
+        Cy.setRadius(22);
+        Cy.setHeight(33);
+        System.out.println(Cy.getRadius());
+        System.out.println(Cy.getHeight());
+
+
+
+    }
+    
+}
