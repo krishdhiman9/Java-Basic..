297f8adbf78fb97ef56132192ec29c884f0dd80b
kd <krishdhiman86@gmail.com>
2026-03-23 18:53:31 +0530

    question

---
+++ oops/accessModifiers/main.java
@@ -0,0 +1,43 @@
+package accessModifiers;
+
+class students{
+    private String name;
+    private int calss;
+    private int age ;
+ 
+    public students(){
+        name = null;
+        calss = 0;
+        age = 0;
+    }
+
+    public void setValues(String s,int c, int a){
+        name = s;
+        calss = c;
+        age = a;
+    }
+
+     public String getName(){
+        return name;
+    }
+     public int getCalss(){
+        return calss;
+     }
+
+     public int getAge(){
+        return age;
+    }
+    
+}
+
+
+public class main {
+    public static void main(String[] args) {
+        students s = new students();
+        s.setValues(null, 12, 13);
+        System.out.println(s.getCalss());
+        System.out.println(s.getAge());
+        System.out.println(s.getName());
+    }
+    
+}
