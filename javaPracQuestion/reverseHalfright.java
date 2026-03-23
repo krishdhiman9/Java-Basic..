fd2c1c1c1a54734310bd6d79aaf65ca1991bf140
kd <krishdhiman86@gmail.com>
2026-03-23 23:19:28 +0530

    star pattern question

---
+++ javaPracQuestion/reverseHalfright.java
@@ -0,0 +1,12 @@
+package javaPracQuestion;
+
+public class reverseHalfright {
+    public static void main(String[] args) {
+        for(int i = 1; i<=5; i++){
+            for(int j=5; j>=i; j--){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
--- javaPracQuestion/squarepattern.java
+++ javaPracQuestion/squarepattern.java
@@ -1,10 +1,10 @@
 package javaPracQuestion;
 
-public class question {
+public class squarepattern {
     public static void main(String[] args) {
         for(int i =1;i<=5; i++){
             for(int j = 1;j<=5;j++){
-                System.out.print("*");
+                System.out.print(" * ");
             }
             System.out.println();
             
