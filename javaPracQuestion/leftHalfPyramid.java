cef17b132b3549f50f1f45b23f318f849b4269ec
kd <krishdhiman86@gmail.com>
2026-03-24 18:17:39 +0530

    prac.quest.

--- javaPracQuestion/leftHalfPyramid.java
+++ javaPracQuestion/leftHalfPyramid.java
@@ -1,11 +1,43 @@
 package javaPracQuestion;
+
 public class leftHalfPyramid {
     public static void main(String[] args) {
-        for(int i =1; i<=5; i++){
-            for(int j = 1; j<=i; j++){
-                System.out.print(" * ");
+        for(int i = 1; i<=5; i++){
+            //print space
+            for(int j = 1; j<=5-i; j++){
+                System.out.print("");
             }
-            System.out.println( );
+            //print stars
+                for(int k=1; k<=i; k++){
+                    System.out.print("* ");
+                    
+                }
+            
+            System.out.println();
         }
     }
 }
+
+
+    
+//     public static void main(String[] args) {
+//         //int n = 5; // number of rows
+
+//         for (int i = 1; i <= 5; i++) {
+
+//             // print spaces
+//             for (int j = 1; j <= 5 - i; j++) {
+//                 System.out.print("  ");
+//             }
+
+//             // print stars
+//             for (int j = 1; j <= i; j++) {
+//                 System.out.print("* ");
+//             }
+
+//             System.out.println(); // next line
+//         }
+//     }
+// }
+
+
---
+++ javaPracQuestion/righttHalfPyramid.java
@@ -0,0 +1,13 @@
+package javaPracQuestion;
+public class righttHalfPyramid {
+    public static void main(String[] args) {
+        for(int i =1; i<=5; i++){
+            for(int j = 1; j<=1; j++);
+
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
+    }
+}
