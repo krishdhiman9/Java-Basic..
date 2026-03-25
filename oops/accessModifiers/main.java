ab567992c80038e80028cc3d7ac7cd1e5771a613
kd <krishdhiman86@gmail.com>
2026-03-25 21:51:30 +0530

    practice question

--- oops/accessModifiers/main.java
+++ oops/accessModifiers/main.java
@@ -15,6 +15,9 @@ class students{
         name = s;
         calss = c;
         age = a;
+        if(age<18){
+            
+        }
     }
 
      public String getName(){
@@ -35,6 +38,7 @@ public class main {
     public static void main(String[] args) {
         students s = new students();
         s.setValues(null, 12, 13);
+        
         System.out.println(s.getCalss());
         System.out.println(s.getAge());
         System.out.println(s.getName());
