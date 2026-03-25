0f17b2caffcd40cfaf306425debcbacf080688e1
kd <krishdhiman86@gmail.com>
2026-03-25 21:57:00 +0530

    practice question

--- javaPracQuestion/righttHalfPyramid.java
+++ javaPracQuestion/righttHalfPyramid.java
@@ -3,11 +3,10 @@ public class righttHalfPyramid {
     public static void main(String[] args) {
         for(int i =1; i<=5; i++){
             for(int j = 1; j<=1; j++);
-
             for(int j = 1; j<=i; j++){
-                System.out.print("*");
+                System.out.print(" * ");
             }
-            System.out.println( );
+            System.out.println();
         }
     }
 }
