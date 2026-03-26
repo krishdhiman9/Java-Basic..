d15f9764204c21a08f210b575c1a8663014660b6
kd <krishdhiman86@gmail.com>
2026-03-26 14:30:34 +0530

    practice q

---
+++ loopingQ/whileloop/sumFirstTen.java
@@ -0,0 +1,15 @@
+package loopingQ.whileloop;
+
+public class sumFirstTen {
+    public static void main(String[] args) {
+        
+        int i=0;
+        int sum = 0;
+        while (i<=10) {
+            sum = sum +i;
+            i++;
+        }
+System.out.println("sum is first 10 natural number is : " + sum);
+    }
+    
+}
