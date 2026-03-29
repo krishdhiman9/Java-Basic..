87c139c5b80fda394e0bb8dcdadde129696c9b3d
kd <krishdhiman86@gmail.com>
2026-03-29 21:40:21 +0530

    practiceQ

--- loopingQ/whileloop/oneToTen.java
+++ loopingQ/whileloop/oneToTen.java
@@ -4,7 +4,7 @@ public class oneToTen {
     public static void main(String[] args) {
         int i=1;
         
-        while(i<=10){
+        while(i<=15){
             System.out.println(i);
             i++;
             
