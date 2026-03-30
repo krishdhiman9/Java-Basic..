d09bf26b437666f6520496e130c83f911d3f29b6
kd <krishdhiman86@gmail.com>
2026-03-30 23:35:02 +0530

    practice Q

--- javaPracQuestion/input1Darray.java
+++ javaPracQuestion/input1Darray.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class input1Darray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         int [] arr = new int [5];
+        
         System.out.println("enter elements");
         
         for(int i = 0;i<arr.length;i++){
