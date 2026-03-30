beeaa197a2053852d55a34a3c7104156f5bffad3
kd <krishdhiman86@gmail.com>
2026-03-30 22:56:10 +0530

    practice question

---
+++ javaPracQuestion/sumArray.java
@@ -0,0 +1,19 @@
+
+package javaPracQuestion;
+import java.util.Scanner;
+
+public class sumArray {public static void main(String[] args) {
+    //Scanner sc = new Scanner(System.in);
+    int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+    int sum = 0;
+    for(int i = 0; i<arr.length;i++){
+        for( int j=0;j<arr.length;j++){
+            //arr [i][j]= sc.nextInt();
+            sum += arr[i][j];
+
+        }
+    }System.out.println(sum);
+
+}
+    
+}
