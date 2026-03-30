1614cd97170d315a8b1306f5bc3b6262a308d428
kd <krishdhiman86@gmail.com>
2026-03-30 23:15:56 +0530

    practice q

--- javaPracQuestion/sumArray.java
+++ javaPracQuestion/sumArray.java
@@ -1,10 +1,9 @@
-
 package javaPracQuestion;
-import java.util.Scanner;
+
 
 public class sumArray {public static void main(String[] args) {
     //Scanner sc = new Scanner(System.in);
-    int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+    int [][] arr = {{1,2,3},{4,0,6},{7,8,9}};
     int sum = 0;
     for(int i = 0; i<arr.length;i++){
         for( int j=0;j<arr.length;j++){
@@ -15,5 +14,4 @@ public class sumArray {public static void main(String[] args) {
     }System.out.println(sum);
 
 }
-    
 }
