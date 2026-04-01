89293a5446d541378b33e55d2ab5d8dc62e1f373
kd <krishdhiman86@gmail.com>
2026-04-01 21:43:41 +0530

    practice q

---
+++ javaPracQuestion/sumClassObject.java
@@ -0,0 +1,15 @@
+package javaPracQuestion;
+class Add{
+    int add(int a, int b){
+    return a+b;
+}
+}
+public class sumClassObject {
+    public static void main(String[] args) {
+        Add s = new Add();
+    int result =s.add(2,2);
+        System.out.print(result);
+    }
+    }
+    
+
