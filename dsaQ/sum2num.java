4726012b809391126ca89af1f565f8480767f943
kd <krishdhiman86@gmail.com>
2026-04-02 21:19:30 +0530

    DSA Q

---
+++ dsaQ/sum2num.java
@@ -0,0 +1,14 @@
+package dsaQ;
+class sum{
+    int add(int a,int b){
+        return a+b;
+    }
+}
+
+public class sum2num {
+    public static void main(String[] args) {
+    sum s = new sum();
+    System.out.println(s.add(3,4));
+}
+    
+}
