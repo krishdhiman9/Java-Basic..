84979698a558f46cce2314c94607f8861385af09
kd <krishdhiman86@gmail.com>
2026-04-02 21:27:09 +0530

    dsa Q

---
+++ dsaQ/largestNum.java
@@ -0,0 +1,21 @@
+package dsaQ;
+class maximum{
+    int max(int a, int b){
+        if(a>b){
+            return a;
+
+        }
+        else {
+            return b;
+        }
+    }
+}
+
+public class largestNum {
+    public static void main(String[] args) {
+        maximum m = new maximum();
+        System.out.println(m.max(4,5));
+        
+    }
+    
+}
