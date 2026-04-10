90db0247ee3cc9ba6b6056768a9c2b2043b34c80
krish dhiman <krishdhiman86@gmail.com>
2026-04-10 12:03:12 +0530

    practice q

--- javaPracQuestion/claculator.java
+++ javaPracQuestion/claculator.java
@@ -6,7 +6,6 @@ public class claculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-
         System.out.print("enter first number: ");
         int a = sc.nextInt();
         System.out.print("enter operator: ");
