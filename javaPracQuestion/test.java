a45503999659404ab858c3cad37d828e92978e41
krish dhiman <krishdhiman86@gmail.com>
2026-04-26 18:42:37 +0530

    prcatice question

--- javaPracQuestion/test.java
+++ javaPracQuestion/test.java
@@ -3,10 +3,10 @@ package javaPracQuestion;
 public class test {
     public static void main() {
         for (int i=1; i<=4; i++){
-            for(int j=3; j>=i; j--){
+            for(int j=2; j<=i; j++){
                 System.out.print(" ");
             }
-            for(int k=1; k<=i;k++){
+            for(int k=4; k>=i;k--){
                 System.out.print("*");
             }System.out.println();
 
