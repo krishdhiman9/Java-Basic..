3ef8cbca8b74b0d3856d6cb22e5ea13bf8d979cc
krish dhiman <krishdhiman86@gmail.com>
2026-04-27 20:59:47 +0530

    prcatice question

--- javaPracQuestion/test.java
+++ javaPracQuestion/test.java
@@ -1,18 +1,47 @@
 package javaPracQuestion;
+import java.util.Scanner;
 
-public class test {
+
+//public class test {
+//    public static void main() {
+//        for (int i=1; i<=4; i++){
+//            for(int j=2; j<=i; j++){
+//                System.out.print(" ");
+//            }
+//
+//            for(int k=4; k>=i;k--){
+//                System.out.print("*");
+//            }System.out.println();
+//
+//        }
+    //int day = 3;
+//        Scanner sc = new Scanner(System.in);
+//        //System.out.println("enter day ");
+//        int day = sc.nextInt();
+//        switch (day) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            default:
+//                System.out.println("Invalid day");
+    //    }
+    //  }
+
+public class test{
     public static void main() {
-        for (int i=1; i<=4; i++){
-            for(int j=2; j<=i; j++){
-                System.out.print(" ");
+        int num = 1;
+        for(int i = 1; i <= 4; i++)
+        {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(num);
             }
-
-            for(int k=4; k>=i;k--){
-                System.out.print("*");
-            }System.out.println();
-
+            System.out.println();
         }
-
     }
 }
-
