44a70497144aac8bac4e435b9c10798631d94165
krish dhiman <krishdhiman86@gmail.com>
2026-05-06 19:44:34 +0530

    practice question

---
+++ dsaQ/newOne.java
@@ -0,0 +1,23 @@
+package dsaQ;
+import java.util.Scanner;
+public class newOne {
+
+    //Q1. print n to 5
+//    public static void main() {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for(int i = n; i<=5; i++){
+//            System.out.println(i);
+//        }
+//    }
+
+//Q2. n to 1
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = n; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+
+}
