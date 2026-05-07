54f1226e6e9ab4d107e4afe540e766ff57bd4da9
krish dhiman <krishdhiman86@gmail.com>
2026-05-07 09:08:59 +0530

    practice question

--- dsaQ/newOne.java
+++ dsaQ/newOne.java
@@ -21,5 +21,26 @@ public class newOne {
 //    }
 
 
+    //Q3. print even and odd number 1 to N.
+//    public static void main() {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        System.out.print("even : ");
+//        for (int i = 1; i<=n; i++) {
+//            if (i % 2 == 0) {
+//                System.out.print(i + " ");
+//            }
+//        }
+//        System.out.println();
+//
+//        System.out.print("odd: ");
+//        for (int j=1; j<=n; j++){
+//            if(j%2!=0){
+//                System.out.print( j + " ");
+//            }
+//
+//        }
+//    }
+
 
 }
