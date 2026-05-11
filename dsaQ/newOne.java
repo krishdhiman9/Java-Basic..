994c27e4c4de99f65495f56ed52170c80cb4625f
krish dhiman <krishdhiman86@gmail.com>
2026-05-11 22:51:41 +0530

    practice question

--- dsaQ/newOne.java
+++ dsaQ/newOne.java
@@ -122,6 +122,29 @@ public class newOne {
 //    }
 
 
+// Sum of Digits
+//public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//            int n = sc.nextInt();
+//            int sum = 0;
+//            while(n > 0) {
+//                sum = sum + (n % 10);
+//                n = n / 10;
+//            }
+//            System.out.println(sum);
+//        }
+//    }
 
+    //Count Digits
+    public static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number: ");
+        int n = sc.nextInt();
+        int count=0;
+        for(int i = n; i>0; i=i/10){
+            count++;
 
+        } System.out.println(count);
+    }
 }
+
